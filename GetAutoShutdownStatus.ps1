2c2d86a13a8e3f38ae8f6849256a0fc7c8a9f00a
Meruem96 <clement.legoupil@gmail.com>
2023-02-27 11:59:15 +0100

    Add script

---
+++ GetAutoShutdownStatus.ps1
@@ -0,0 +1,32 @@
+# =======================================================
+# NAME: GetAutoShutdownStatus.ps1
+# AUTHOR: LEGOUPIL, Clement, Entreprise
+# DATE: 27/02/2023
+# =======================================================
+
+$vmliststatus = @()
+
+foreach ($vm in Get-AzVm){
+    try  {
+        $subscriptionId = ($vm.Id -split "/")[2]
+        $shutdownresource = Get-AzResource `
+                -ResourceId "/subscriptions/$subscriptionId/resourceGroups/$($vm.ResourceGroupName)/providers/microsoft.devtestlab/schedules/shutdown-computevm-$($vm.Name)" `
+                -ErrorAction Stop
+
+        $vmliststatus += New-Object psobject -Property @{
+                "VMName" = $vm.Name
+                "ShutdownStatus" = $shutdownResource.Properties.status
+        }
+    }
+    catch {
+        $vmliststatus += New-Object psobject -Property @{
+                "VMName" = $vm.Name
+                "ShutdownStatus" = $_.Exception.Message
+        }
+
+    }
+    
+}
+
+$vmliststatus
+
