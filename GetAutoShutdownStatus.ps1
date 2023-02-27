6310f1196fa8c8f11407ba060f0c78a2f15e7a35
Meruem96 <clement.legoupil@gmail.com>
2023-02-27 14:51:13 +0100

    add tags

--- GetAutoShutdownStatus.ps1
+++ GetAutoShutdownStatus.ps1
@@ -4,6 +4,10 @@
 # DATE: 27/02/2023
 # =======================================================
 
+#
+# Add tags associated for contact
+#
+
 $vmliststatus = @()
 
 foreach ($vm in Get-AzVm){
@@ -16,12 +20,14 @@ foreach ($vm in Get-AzVm){
         $vmliststatus += New-Object psobject -Property @{
                 "VMName" = $vm.Name
                 "ShutdownStatus" = $shutdownResource.Properties.status
+                "Tags" = $vm.Tags
         }
     }
     catch {
         $vmliststatus += New-Object psobject -Property @{
                 "VMName" = $vm.Name
                 "ShutdownStatus" = "Not configured"
+                "Tags" = $vm.Tags
         }
 
     }
