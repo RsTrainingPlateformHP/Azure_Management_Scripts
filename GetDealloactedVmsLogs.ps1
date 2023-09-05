eff12fdca6504de6488d3e4256c137db92f72b4b
RsTrainingPlateformHP <121814757+RsTrainingPlateformHP@users.noreply.github.com>
2023-09-05 16:45:16 +0200

    Add files via upload

---
+++ GetDealloactedVmsLogs.ps1
@@ -0,0 +1,37 @@
+# =======================================================
+# NAME: GetDealloactedVmsLogs.ps1
+# AUTHOR: LEGOUPIL, Clement, Entreprise
+# DATE: 10/02/2023
+# =======================================================
+
+
+$LogsReport = @()
+$MonthAgoDdate = (Get-Date).AddMonths(-1)
+$vms = Get-AzureRmVm | Where-Object { $_.Name -notlike "*training*labs*" }
+$counter=0
+foreach($vm in $vms)
+{
+    $counter++
+    write-progress -Activity 'Processing Vms' -Status "Processing $($counter) of $($vms.count) - ($($vm.Name))" -CurrentOperation $vm -PercentComplete (($counter / $vms.count)*100)
+    
+    $logs = Get-AzLog -StartTime $MonthAgoDdate -DetailedOutput -ResourceId $vm.Id -WarningAction 0| Where-Object {$_.Authorization.Action -like 'Microsoft.Compute/virtualMachines/deallocate/action*'}
+    foreach ($log in $logs)
+    {
+        $date = $log.EventTimestamp -split " "
+        $date = $date[0] -split "/"
+        $date = "$($date[1])/$($date[0])/$($date[2])"
+        $LogsReport += New-Object psobject -Property @{
+            "VMName" = $vm.Name
+            "VMId" = $vm.Id
+            "VMSize" = $vm.HardwareProfile.VmSize
+            "OperationName" = $log.OperationName
+            "LogTimeStamp" = $date
+
+        }
+    }
+}
+
+
+$LogsReport = $LogsReport | Select-Object -Property *, @{Name="Hash";Expression={$_ | Out-String}} | Sort-Object -Property Hash -Unique | Select-Object -Property * -ExcludeProperty Hash
+$nowDate = Get-Date -Format "dd-MM-yyyy"
+$LogsReport | Export-Csv "report-$nowDate.csv"
---
+++ GetVmPublicIp.ps1
@@ -0,0 +1,32 @@
+# =======================================================
+# NAME: GetVmPublicIp.ps1
+# AUTHOR: FAVROT, Jean-Baptiste, Entreprise
+# DATE: 31/05/2022
+#
+# Lists all VMs with corresponding public IPs. Takes a string with the name of the VMs you want to match in input
+#
+# Usage : ./GetVmPublicIp.ps1 "splunk" "splunkPubIps" --> output : splunkPubIps.csv
+#
+# =======================================================
+
+param 
+(
+     [string]$VMName,
+     [string][Parameter(Mandatory)]$OutputName
+)
+
+function GetPublicIP ($VMName) {
+     $publicIPs = Get-AzPublicIpAddress | where-object -Property Name -Match "$VMName"
+
+     Write-Output 'Resource Group,Public IP Name,Public IP Address'
+     
+     foreach($publicIP in $publicIPs) {
+          Write-Output "$($publicIP.ResourceGroupName),$($publicIP.Name),$($publicIP.IpAddress)"
+         }
+}
+
+
+
+GetPublicIP($VMName) | Out-File -FilePath ".\$OutputName.csv" -NoClobber
+
+cat "$OutputName.csv"
